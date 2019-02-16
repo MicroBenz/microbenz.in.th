d9eb53f28f78c22e83dad01533bf0ce657b4a661
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2019-02-16 14:27:36 +0700

    Fluid image not fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -114,7 +114,7 @@ export const pageQuery = graphql`
             featuredImage {
               childImageSharp {
                 fluid(maxWidth: 300, quality: 80) {
-                  ...GatsbyImageSharpFixed_withWebp
+                  ...GatsbyImageSharpFluid_withWebp
                 }
               }
             }
