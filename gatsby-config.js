9efcdfcc2a2215ce07b935ad31b86059b4078c1a
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2019-04-28 20:14:07 +0700

    Update images

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,7 @@ module.exports = {
             },
           },
           'gatsby-remark-prismjs',
-          'gatsby-remark-copy-linked-files',
+          // 'gatsby-remark-copy-linked-files',
           'gatsby-remark-smartypants',
           {
             resolve: 'gatsby-remark-external-links',
